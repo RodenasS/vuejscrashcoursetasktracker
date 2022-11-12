e2f7f73d918db03c13ffd6be2e52f635b5d3683e
RodenasS <rodenas.sut@gmail.com>
2022-11-13 00:09:54 +0200

    TaskTracker

---
+++ vjscrash/src/components/Header-Item.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonItem @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"> {{text}}></ButtonItem>
+  </header>
+</template>
+
+<script>
+import ButtonItem from "@/components/Button-Item";
+
+export default {
+  name: 'Header-Item',
+  components: {ButtonItem},
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  component : {
+    ButtonItem
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ vjscrash/src/components/Tasks-Item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <TaskItem @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id) " :task="task"/>
+  </div>
+
+</template>
+<script>
+import TaskItem from "@/components/Task-Item";
+
+export default {
+  name: 'TasksItem',
+  props: {
+    tasks: Array
+  },
+  components: {
+    TaskItem
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
